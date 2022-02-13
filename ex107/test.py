c2567c92217783c9ab5d25fe4c2ed7797e01b361
Fernando Dornelles Rocha <89082686+dornellesfr@users.noreply.github.com>
2022-02-13 17:34:46 -0300

    Delete ex107 directory

--- ex107/test.py
+++
@@ -1,6 +0,0 @@
-import coin
-n = float(input('Type the price: R$ '))
-print(f'The half of {n} is R$ {coin.divid(n):.2f}')
-print(f'The twice of {n} is R$ {coin.twice(n):.2f}')
-print(f'The increased 10% in {n} is R$ {coin.increase(n, 10):.2f}')
-print(f'The reduce 5% in {n} is R$ {coin.reduce(n, 5):.2f}')
